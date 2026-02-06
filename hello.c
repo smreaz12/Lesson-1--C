42a45f9b879f8e16b2d8462fa766b4977ba6e80d
smreaz12 <smreaz555@gmail.com>
2026-02-06 17:49:24 +0600

    hello C

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    //printf("hello reaz");
+    //return 0;
+
+    // int age = 23;
+    // printf("age is: %d",age);
+    // return 0;
+
+    // float pi = 3.1416;
+    // printf("pi value is: %f",pi);
+    // return 0;
+
+    char star = '*';
+    printf("star is: %c",star);
+    return 0;
+}
+
